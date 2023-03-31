9d4b557bfbebee3e6b63671b7e61f8333e2ae47e
Polycarp Okock <deaspo@users.noreply.github.com>
2023-03-31 20:26:50 +0200

    - refactor selector

--- apps/NgBookingSystem/src/features/bookings/store/selectors/bookings.selectors.ts
+++ apps/NgBookingSystem/src/features/bookings/store/selectors/bookings.selectors.ts
@@ -9,8 +9,24 @@ import {
     selectAdapterBookingIds
 } from "../reducer/booking.reducer";
 
-export const selectBookingState = createFeatureSelector<BookingState>(bookingFeatureKey);
-export const selectAllBookings = createSelector(selectBookingState, (state: BookingState) => state.bookings.slice().sort((a, b) => b.postedDate.localeCompare(a.postedDate)));
+//export const selectBookingState = createFeatureSelector<BookingState>(bookingFeatureKey);
+//export const selectAllBookings = createSelector(selectBookingState, (state: BookingState) =>
+// state.bookings.slice().sort((a, b) => b.postedDate.localeCompare(a.postedDate)));
+/*export const selectBookingById = (bookingId: string) => createSelector(
+ selectAllBookings,
+ (bookings: Booking[]) => bookings.find(booking => booking.id === bookingId)
+ );*/
+
+// From Adapter
+export const selectBookingState = createFeatureSelector<BookingAdapterState>(bookingFeatureKey);
+export const selectBookingIds = createSelector(selectBookingState, selectAdapterBookingIds);
+export const selectBookingEntities = createSelector(selectBookingState, selectAdapterBookingEntities);
+export const selectAllBookings = createSelector(selectBookingState, selectAdapterAllBookings);
+export const selectAllSortedBookings = createSelector(
+    selectAllBookings,
+    (bookings: Booking[]) => bookings.sort(
+        (a: Booking, b: Booking) => b.postedDate.localeCompare(a.postedDate))
+)
 export const selectBookingById = (bookingId: string) => createSelector(
     selectAllBookings,
     (bookings: Booking[]) => bookings.find(booking => booking.id === bookingId)
